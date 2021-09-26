6a94bc45e1f7a228a3d81702783a0b1cafa83ce0
xionglaifu <xionglaifu@formssi.com>
2021-09-26 09:33:22 +0800

    update

--- api/article.js
+++ api/article.js
@@ -3,69 +3,87 @@
  * @Author: xionglaifu
  * @Date: 2021-06-23 14:04:32
  * @LastEditors: xionglaifu
- * @LastEditTime: 2021-09-25 21:57:33
+ * @LastEditTime: 2021-09-26 09:31:32
  * @company: formssi
  */
-import { request } from "@/plugins/request";
+import { request } from '@/plugins/request'
 
 // 获取公共文章列表
 export const getArticles = params => {
   return request({
-    method: "GET",
-    url: "/api/articles",
+    method: 'GET',
+    url: '/api/articles',
     params
-  });
-};
+  })
+}
 
 // 获取公共文章列表
 export const getYourFeedArticles = params => {
   return request({
-    method: "GET",
-    url: "/api/articles/feed",
+    method: 'GET',
+    url: '/api/articles/feed',
     params
     // headers: {
     //   // 添加用户身份，数据格式：Token空格Token数据
     //   Authorization: `Token eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NDgxMTYsInVzZXJuYW1lIjoibHB6OTk5IiwiZXhwIjoxNTk3NzQxNTA4fQ.2yO8Fss4hYnvsIN2UYHsutQ1hmYqSSAA-UrIRnP4DOY`
     // }
-  });
-};
+  })
+}
 
 // 添加点赞
 export const addFavorite = slug => {
   return request({
-    method: "POST",
+    method: 'POST',
     url: `/api/articles/${slug}/favorite`
-  });
-};
+  })
+}
 
 // 取消点赞
 export const deleteFavorite = slug => {
   return request({
-    method: "DELETE",
+    method: 'DELETE',
     url: `/api/articles/${slug}/favorite`
-  });
-};
+  })
+}
 
 // 获取文章详情
 export const getArticle = slug => {
   return request({
-    method: "GET",
+    method: 'GET',
     url: `/api/articles/${slug}`
-  });
-};
+  })
+}
 
 // 获取文章评论
 export const getComments = slug => {
   return request({
-    method: "GET",
+    method: 'GET',
     url: `/api/articles/${slug}/comments`
-  });
-};
+  })
+}
 
 // 获取文章标签列表
 export const getTags = () => {
   return request({
-    method: "GET",
-    url: "/api/tags"
-  });
-};
+    method: 'GET',
+    url: '/api/tags'
+  })
+}
+
+//发表文章
+export const createArticles = data => {
+  return request({
+    method: 'post',
+    url: '/api/articles',
+    data
+  })
+}
+
+//修改文章
+export const updateArticles = slug => {
+  return request({
+    method: 'put',
+    url: `/api/articles/${slug}`,
+    data
+  })
+}
--- api/user.js
+++ api/user.js
@@ -1,5 +1,12 @@
+/*
+ * @Description:
+ * @Author: xionglaifu
+ * @Date: 2021-09-26 09:15:50
+ * @LastEditors: xionglaifu
+ * @LastEditTime: 2021-09-26 09:32:00
+ * @company: formssi
+ */
 import { request } from '@/plugins/request'
-
 
 // 用户登录
 export const login = data => {
@@ -16,5 +23,22 @@ export const register = data => {
     method: 'POST',
     url: '/api/users',
     data
+  })
+}
+
+//修改用户信息
+export const updateUser = data => {
+  return request({
+    method: 'post',
+    url: '/api/user',
+    data
+  })
+}
+
+//获取个人资料
+export const getProfile = userName => {
+  return request({
+    method: 'get',
+    url: `/api/profiles/${userName}`
   })
 }
